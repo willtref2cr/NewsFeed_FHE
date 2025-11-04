186b85547daad1f1761ec25212a2afa2b743ee46
willtref2cr <willtref2cr@users.noreply.github.com>
2025-11-04 17:16:16 +0000

    Add bridge-lib-subscriptions.scss, decorator-simple-user.java

---
+++ frontend/web/src/admin-product-module/decorator-simple-user.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/newsfeed_fhe/frontend/web/src/admin-product-module/decorator-simple-user.java
+// Generated at: 2025-11-27T12:25:21.647Z
+
